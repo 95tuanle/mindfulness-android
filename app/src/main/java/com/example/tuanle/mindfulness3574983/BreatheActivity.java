32066bab0f7081fbb0a4fe1786d1b9afd6180d3d
Tuan Le <lenguyenanhtuan95@gmail.com>
2018-11-19 14:17:12 +0700

    Finalized Stop Watch

--- app/src/main/java/com/example/tuanle/mindfulness3574983/BreatheActivity.java
+++ app/src/main/java/com/example/tuanle/mindfulness3574983/BreatheActivity.java
@@ -3,7 +3,6 @@ package com.example.tuanle.mindfulness3574983;
 import android.arch.lifecycle.Observer;
 import android.os.SystemClock;
 import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -13,7 +12,6 @@ import android.widget.Chronometer;
 import android.widget.TextView;
 
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 
@@ -24,22 +22,27 @@ public class BreatheActivity extends AppCompatActivity {
     TextView today;
     TextView thisWeek;
     TextView thisMonth;
-
-
-
+    SessionRepository sessionRepository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_breathe);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
-
         stopWatch = findViewById(R.id.stopWatch);
         startButton = findViewById(R.id.start);
         endButton = findViewById(R.id.end);
+        today = findViewById(R.id.today);
+        thisWeek = findViewById(R.id.thisWeek);
+        thisMonth = findViewById(R.id.thisMonth);
         endButton.setEnabled(false);
-
-
+        sessionRepository = new SessionRepository(getApplicationContext());
+        sessionRepository.getAllSessions().observe(this, new Observer<List<Session>>() {
+            @Override
+            public void onChanged(@Nullable List<Session> sessions) {
+                updateDWMTexts(sessions, Calendar.getInstance());
+            }
+        });
     }
 
     @Override
@@ -51,8 +54,6 @@ public class BreatheActivity extends AppCompatActivity {
     public void onStartClick(View view) {
         stopWatch.setBase(SystemClock.elapsedRealtime());
         stopWatch.start();
-
-
         startButton.setEnabled(false);
         endButton.setEnabled(true);
     }
@@ -61,19 +62,71 @@ public class BreatheActivity extends AppCompatActivity {
         long sessionDuration = SystemClock.elapsedRealtime() - stopWatch.getBase();
         stopWatch.stop();
         stopWatch.setBase(SystemClock.elapsedRealtime());
-        SessionRepository sessionRepository = new SessionRepository(getApplicationContext());
-        sessionRepository.addSession(new Session(sessionDuration, Calendar.getInstance().getTime()));
+        final Calendar calendar = Calendar.getInstance();
+        sessionRepository.addSession(new Session(sessionDuration, calendar.getTime()));
         sessionRepository.getAllSessions().observe(this, new Observer<List<Session>>() {
             @Override
             public void onChanged(@Nullable List<Session> sessions) {
-                updateDWMTexts(sessions);
+                updateDWMTexts(sessions, calendar);
             }
         });
         startButton.setEnabled(true);
         endButton.setEnabled(false);
     }
 
-    private void updateDWMTexts(List<Session> sessions) {
+    private void updateDWMTexts(List<Session> sessions, Calendar calendar) {
+        int dayCurrent = calendar.get(Calendar.DATE);
+        int weekCurrent = calendar.get(Calendar.WEEK_OF_MONTH);
+        int monthCurrent = calendar.get(Calendar.MONTH);
+        int yearCurrent = calendar.get(Calendar.YEAR);
+        long dayDuration = 0;
+        long weekDuration = 0;
+        long monthDuration = 0;
+        for (Session session:
+             sessions) {
+            Calendar calendarFromSession = Calendar.getInstance();
+            calendarFromSession.setTime(session.getDateAdded());
+            long sessionDuration = session.getDuration();
+            if (calendarFromSession.get(Calendar.YEAR) == yearCurrent) {
+                if (calendarFromSession.get(Calendar.MONTH) == monthCurrent) {
+                    monthDuration += sessionDuration;
+                    if (calendarFromSession.get(Calendar.WEEK_OF_MONTH) == weekCurrent) {
+                        weekDuration += sessionDuration;
+                        if (calendarFromSession.get(Calendar.DATE) == dayCurrent) {
+                            dayDuration += sessionDuration;
+                        }
+                    }
+                }
+            }
+        }
+        today.setText(buildDuration(dayDuration));
+        thisWeek.setText(buildDuration(weekDuration));
+        thisMonth.setText(buildDuration(monthDuration));
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sessionRepository.getAllSessions().observe(this, new Observer<List<Session>>() {
+            @Override
+            public void onChanged(@Nullable List<Session> sessions) {
+                updateDWMTexts(sessions, Calendar.getInstance());
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        long sessionDuration = SystemClock.elapsedRealtime() - stopWatch.getBase();
+        sessionRepository.addSession(new Session(sessionDuration, Calendar.getInstance().getTime()));
+    }
+
+    private String buildDuration(long duration) {
+        long millis = duration % 1000;
+        long second = (duration / 1000) % 60;
+        long minute = (duration / (1000 * 60)) % 60;
+        long hour = (duration / (1000 * 60 * 60)) % 24;
+        return String.format("%02d:%02d:%02d.%d", hour, minute, second, millis);
     }
 }
